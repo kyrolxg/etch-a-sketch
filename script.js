eda083d48091e1ca3da4e563d102dce0daee0a4b
kyro <71367144+kyrolxg@users.noreply.github.com>
2025-12-15 23:05:41 +0530

    Initial 16x16 grid setup completed

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-const containerDiv = document.querySelector("#container");
+const container = document.querySelector("#container");
+
+
+const size = 16*16;
+for(let i = 0; i<size; i++) {
+    const cells = document.createElement("div");
+    cells.className = "cells";
+    container.appendChild(cells);
+}
